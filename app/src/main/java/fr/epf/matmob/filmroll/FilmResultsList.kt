00850f4ee1f675d734fd4aec6b899ec6c7e2dfe3
Émile <emile.royer@epfedu.fr>
2023-06-09 19:43:46 +0200

    Make the vertical film list reusable

--- app/src/main/java/fr/epf/matmob/filmroll/FilmResultsList.kt
+++ app/src/main/java/fr/epf/matmob/filmroll/FilmResultsList.kt
@@ -1,38 +1,25 @@
 package fr.epf.matmob.filmroll
 
-import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.unit.dp
-import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
-import com.bumptech.glide.integration.compose.GlideImage
-import fr.epf.matmob.filmroll.model.LiteFilm
 import fr.epf.matmob.filmroll.state.FilmApplication
 import fr.epf.matmob.filmroll.state.FilmViewModel
 import fr.epf.matmob.filmroll.state.FilmViewModelFactory
+import fr.epf.matmob.filmroll.ui.components.VerticalFilmList
 import fr.epf.matmob.filmroll.ui.theme.FilmrollTheme
 
 private const val TAG = "FilmResultsList"
@@ -57,11 +44,7 @@ class FilmResultsList : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val foundFilms by viewModel.foundFilms.observeAsState()
-                    Column {
-                        Text(text = "Texte cherché : $query")
-                        foundFilms?.let { ResultsList(filmList = it) }
-                    }
+                    SearchScreenStructure(viewModel = viewModel, query = query!!)
                 }
             }
         }
@@ -69,58 +52,15 @@ class FilmResultsList : ComponentActivity() {
 }
 
 @Composable
-fun ResultsList(filmList: List<LiteFilm>) {
-    LazyColumn {
-        items(filmList) {
-            FilmResult(film = it)
-        }
+fun SearchScreenStructure(viewModel: FilmViewModel, query: String) {
+    val foundFilms by viewModel.foundFilms.observeAsState()
+    Scaffold(topBar = { ResultsTopBar(query = query) }) { padVal ->
+        foundFilms?.let { VerticalFilmList(filmList = it, modifier = Modifier.padding(padVal)) }
     }
 }
 
-@OptIn(ExperimentalGlideComposeApi::class)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun FilmResult(film: LiteFilm) {
-    val activity = LocalContext.current as Activity
-    Surface(
-        tonalElevation = 2.dp,
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(4.dp)
-            .height(142.dp)
-            .clickable {
-                activity.startActivity(
-                    Intent(
-                        activity,
-                        FilmCardActivity::class.java
-                    ).putExtra("TMDBFilmId", film.tmdbId)
-                )
-            },
-        shape = MaterialTheme.shapes.large
-    ) {
-        Row {
-            GlideImage(
-                model = if (film.posterPath.isEmpty()) {
-                    "https://placehold.co/342x513.png"
-                } else {
-                    "https://image.tmdb.org/t/p/w342${film.posterPath}"
-                },
-                contentDescription = "${film.title}'s poster image",
-                modifier = Modifier.width(96.dp)
-            )
-            Spacer(modifier = Modifier.width(8.dp))
-            Column {
-                Text(text = film.title, style = MaterialTheme.typography.titleMedium)
-                Text(
-                    text = film.releaseDate,
-                    style = MaterialTheme.typography.bodyMedium
-                )
-                Text(
-                    text = film.synopsis,
-                    overflow = TextOverflow.Ellipsis,
-                    style = MaterialTheme.typography.bodyMedium
-                )
-            }
-        }
-
-    }
+fun ResultsTopBar(query: String) {
+    TopAppBar(title = { Text(text = "Query: $query", style = MaterialTheme.typography.headlineSmall) })
 }
---
+++ app/src/main/java/fr/epf/matmob/filmroll/ui/components/FilmList.kt
@@ -0,0 +1,82 @@
+package fr.epf.matmob.filmroll.ui.components
+
+import android.app.Activity
+import android.content.Intent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
+import fr.epf.matmob.filmroll.FilmCardActivity
+import fr.epf.matmob.filmroll.model.LiteFilm
+
+@OptIn(ExperimentalGlideComposeApi::class)
+@Composable
+fun FilmListItem(film: LiteFilm) {
+    val activity = LocalContext.current as Activity
+    Surface(
+        tonalElevation = 2.dp,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(4.dp)
+            .height(142.dp)
+            .clickable {
+                activity.startActivity(
+                    Intent(
+                        activity,
+                        FilmCardActivity::class.java
+                    ).putExtra("TMDBFilmId", film.tmdbId)
+                )
+            },
+        shape = MaterialTheme.shapes.large
+    ) {
+        Row {
+            GlideImage(
+                model = if (film.posterPath.isEmpty()) {
+                    "https://placehold.co/342x513.png"
+                } else {
+                    "https://image.tmdb.org/t/p/w342${film.posterPath}"
+                },
+                contentDescription = "${film.title}'s poster image",
+                modifier = Modifier.width(96.dp)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Column {
+                Text(text = film.title, style = MaterialTheme.typography.titleMedium)
+                Text(
+                    text = film.releaseDate,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+                Text(
+                    text = film.synopsis,
+                    overflow = TextOverflow.Ellipsis,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun VerticalFilmList(filmList: List<LiteFilm>, modifier: Modifier = Modifier) {
+    LazyColumn(modifier = modifier) {
+        items(filmList) {
+            FilmListItem(film = it)
+        }
+    }
+}
