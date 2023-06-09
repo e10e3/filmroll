e56c13958b168f6ad3ba356022a26a5a4bf90d8b
Émile <emile.royer@epfedu.fr>
2023-06-09 20:08:43 +0200

    Improve Film carousel padding

--- app/src/main/java/fr/epf/matmob/filmroll/ui/components/FilmCarousel.kt
+++ app/src/main/java/fr/epf/matmob/filmroll/ui/components/FilmCarousel.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -28,39 +29,47 @@ import fr.epf.matmob.filmroll.model.LiteFilm
 fun FilmCarousel(films: List<LiteFilm>, title: String) {
     val activity = LocalContext.current as Activity
     val columnWidth = 111.dp
-    Column {
-        Text(
-            text = title,
-            style = MaterialTheme.typography.titleMedium,
-            modifier = Modifier.padding(bottom = 4.dp)
-        )
-        LazyRow(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
-            items(films) {
-                Column(modifier = Modifier
-                    .clickable {
-                        activity.startActivity(
-                            Intent(
-                                activity, FilmCardActivity::class.java
-                            ).putExtra("TMDBFilmId", it.tmdbId)
+    Surface(
+        color = MaterialTheme.colorScheme.primaryContainer,
+        modifier = Modifier.padding(horizontal = 4.dp),
+        shape = MaterialTheme.shapes.medium
+    ) {
+        Column {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier.padding(bottom = 4.dp, top = 8.dp, start = 8.dp)
+            )
+            LazyRow(horizontalArrangement = Arrangement.spacedBy(8.dp), modifier = Modifier.padding(horizontal = 2.dp)) {
+                items(films) {
+                    Column(modifier = Modifier
+                        .clickable {
+                            activity.startActivity(
+                                Intent(
+                                    activity, FilmCardActivity::class.java
+                                ).putExtra("TMDBFilmId", it.tmdbId)
+                            )
+                        }
+                        .width(columnWidth)) {
+                        GlideImage(
+                            model = if (it.posterPath.isEmpty()) {
+                                "https://placehold.co/342x513.png"
+                            } else {
+                                "https://image.tmdb.org/t/p/w342${it.posterPath}"
+                            },
+                            contentDescription = "${it.title}'s poster image",
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(175.dp)
+                        )
+                        Text(
+                            text = it.title,
+                            style = MaterialTheme.typography.labelSmall,
+                            modifier = Modifier.height(50.dp),
+                            overflow = TextOverflow.Ellipsis,
+                            maxLines = 3
                         )
                     }
-                    .width(columnWidth)) {
-                    GlideImage(
-                        model = if (it.posterPath.isEmpty()) {
-                            "https://placehold.co/342x513.png"
-                        } else {
-                            "https://image.tmdb.org/t/p/w342${it.posterPath}"
-                        },
-                        contentDescription = "${it.title}'s poster image",
-                        modifier = Modifier.fillMaxWidth().height(175.dp)
-                    )
-                    Text(
-                        text = it.title,
-                        style = MaterialTheme.typography.labelSmall,
-                        modifier = Modifier.height(50.dp),
-                        overflow = TextOverflow.Ellipsis,
-                        maxLines = 3
-                    )
                 }
             }
         }
