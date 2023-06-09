a7ff878a7f9711b3575d9eddbfa0ec913e14be38
Émile <emile.royer@epfedu.fr>
2023-06-09 13:58:45 +0200

    Add a top and bottom bar to Home Activity
    They don't do much for now

--- app/src/main/java/fr/epf/matmob/filmroll/MainActivity.kt
+++ app/src/main/java/fr/epf/matmob/filmroll/MainActivity.kt
@@ -7,20 +7,26 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.QrCodeScanner
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -30,10 +36,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import fr.epf.matmob.filmroll.state.FilmApplication
 import fr.epf.matmob.filmroll.state.FilmViewModel
@@ -56,7 +65,7 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
-                    DisplayHomeScreen(viewModel = viewModel, this)
+                    HomeStructure(viewModel = viewModel, context = this)
                 }
             }
         }
@@ -64,11 +73,19 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun DisplayHomeScreen(viewModel: FilmViewModel, context: Context) {
+fun HomeStructure(viewModel: FilmViewModel, context: Context) {
+    Scaffold(bottomBar = { AppNavigationBar() }, topBar = { HomeTopBar() }) { padValues ->
+        DisplayHomeScreen(
+            viewModel = viewModel, context = context, modifier = Modifier.padding(padValues)
+        )
+    }
+}
+
+@Composable
+fun DisplayHomeScreen(viewModel: FilmViewModel, context: Context, modifier: Modifier = Modifier) {
     viewModel.getPopularFilms()
-    LazyColumn {
+    LazyColumn(modifier = modifier) {
         item {
-            Spacer(modifier = Modifier.height(128.dp))
             val (value, onValueChange) = remember { mutableStateOf("") }
             TextField(
                 value = value,
@@ -87,22 +104,20 @@ fun DisplayHomeScreen(viewModel: FilmViewModel, context: Context) {
                     )
                 },
                 keyboardActions = KeyboardActions(onSearch = {
-                    val intent =
-                        Intent(context, FilmResultsList::class.java)
+                    val intent = Intent(context, FilmResultsList::class.java)
                     intent.putExtra("searchQuery", value)
                     context.startActivity(intent)
                 }),
                 keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(16.dp)
-                    .shadow(elevation = 8.dp),
+                    .padding(start = 16.dp, end = 16.dp, top = 64.dp, bottom = 96.dp),
+                // .shadow(elevation = 8.dp),
                 colors = TextFieldDefaults.colors(
                     focusedIndicatorColor = Color.Transparent,
                     unfocusedIndicatorColor = Color.Transparent
                 )
             )
-            Spacer(modifier = Modifier.height(96.dp))
 
             val popularFilms by viewModel.popularFilms.observeAsState()
             popularFilms?.let {
@@ -115,5 +130,45 @@ fun DisplayHomeScreen(viewModel: FilmViewModel, context: Context) {
                 Text(text = "Scan a QR code")
             }
         }
+    }
+}
+
+data class NavItem(val label: String, val icon: ImageVector)
+
+@Composable
+fun AppNavigationBar() {
+    var selectedItem by remember { mutableStateOf(0) }
+    val items = listOf(
+        NavItem("Home", Icons.Default.Home),
+        NavItem("Favourites", Icons.Default.Favorite),
+        NavItem("QR Scan", Icons.Default.QrCodeScanner)
+    )
+
+    NavigationBar {
+        items.forEachIndexed { index, item ->
+            NavigationBarItem(icon = { Icon(item.icon, contentDescription = item.label) },
+                label = { Text(item.label, style = MaterialTheme.typography.labelSmall) },
+                selected = selectedItem == index,
+                onClick = { selectedItem = index })
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeTopBar() {
+    CenterAlignedTopAppBar(title = {
+        Text(
+            text = stringResource(id = R.string.app_name),
+            style = MaterialTheme.typography.titleMedium
+        )
+    })
+}
+
+@Preview
+@Composable
+fun NavigationBarPreview() {
+    FilmrollTheme {
+        AppNavigationBar()
     }
 }
